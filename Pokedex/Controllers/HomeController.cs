fa1cbc6a39e53bcc9aca2678681908b6938bc78f
itsquelc <raquelnunescirino@gmail.com>
2024-03-13 14:46:05 -0300

    finalizado a 142

--- Pokedex/Controllers/HomeController.cs
+++ Pokedex/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Pokedex.Models;
 
@@ -15,9 +16,43 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        List<Pokemon> pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = [];
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        ViewData["Tipos"] = tipos;
+        return View(pokemons);
     }
 
+    public IActionResult Details(int id)
+    {
+        List<Pokemon> pokemons = [];
+        using (StreamReader leitor = new("Data\\pokemons.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            pokemons = JsonSerializer.Deserialize<List<Pokemon>>(dados);
+        }
+        List<Tipo> tipos = [];
+        using (StreamReader leitor = new("Data\\tipos.json"))
+        {
+            string dados = leitor.ReadToEnd();
+            tipos = JsonSerializer.Deserialize<List<Tipo>>(dados);
+        }
+        ViewData["Tipos"] = tipos;
+        var pokemon = pokemons
+            .Where(p => p.Numero == id)
+            .FirstOrDefault();
+        return  View(pokemon);
+    }
+    
     public IActionResult Privacy()
     {
         return View();
--- Pokedex/Models/Pokemon.cs
+++ Pokedex/Models/Pokemon.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace Pokedex.Models;
 
-namespace Pokedex.Models
-{
     public class Pokemon
-    {
-        public int Numero { get; set; }
-        public string Nome { get; set; }
-        public string Descricao { get; set; }
-        public string Especie { get; set; }
-        public List<string> Tipo { get; set; }
-        public double Altura { get; set; }
-        public double Peso { get; set; }
-        public string Imagem { get; set; }
-    }
-}
+   {
+   public int Numero { get; set; }   
+   public string Nome { get; set; }
+   public string Descricao { get; set; }
+   public string Especie { get; set; }
+   public List<string> Tipo { get; set; } = [];
+   public double Altura { get; set; }
+   public double Peso { get; set; }
+   public string Imagem { get; set; }
+   }
